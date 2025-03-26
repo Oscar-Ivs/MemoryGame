826058b499946eb450f54bfc9a82cfc2481fe9b0
Oscar-Ivs <hk57agob@students.codeinstitute.net>
2025-03-26 16:07:40 +0000

    Create memory game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,26 @@ const cell = document.createElement('div');
 cell.dataset.index = i;
 gameGrid.appendChild(cell);
 }
+
+//Starting the game
+function startGame() {
+    if (awaitingAnswer) return; // Not possible to start a new game while waiting for the answer
+
+    const cells = document.querySelectorAll('#game-grid div');
+    let.randomIndex = Math.floor(Math.random() * cells.length);
+    activeCell = cells[randomIndex];
+
+    //Highlight the active cell for 500ms
+    activeCell.classList.add('active');
+    awaitingAnswer = true;
+
+    setTimeout(() => {
+        activeCell.classList.remove('active');
+    } , 500);
+
+    //Only allow one click per round
+    cells.forEach(cell => {
+        cell.onClick = handleCellClick;
+    });
+}
+
